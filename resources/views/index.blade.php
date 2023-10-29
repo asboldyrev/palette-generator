693d3258553c45adbf75605a021355cbcda6437b
asboldyrev <angelcorpc@ya.ru>
2023-10-29 18:11:57 +0700

    Правки стилей

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,18 +10,21 @@
 <body>
 	<div class="container">
 		<h1>Генератор палитры по картинке</h1>
-		<form class="form" action="{{ route('store') }}" method="post" enctype="multipart/form-data">
-			<label class="label" for="versionSelect">Версия</label>
-			<select class="select" id="versionSelect" name="version">
-				<option value="1">Оригинальная</option>
-				<option value="2">V2</option>
-			</select>
-			<label class="label" for="fileInput">Файл для обработки</label>
-			<input class="file-input" id="fileInput" type="file" name="file">
-			<button class="button">Загрузить</button>
-		</form>
 
-		@yield('result')
+		<div class="content">
+			<form class="form" action="{{ route('store') }}" method="post" enctype="multipart/form-data">
+				<label class="label" for="versionSelect">Версия</label>
+				<select class="select" id="versionSelect" name="version">
+					<option value="1">V1 (Оригинальная)</option>
+					<option value="2">V2</option>
+				</select>
+				<label class="label" for="fileInput">Файл для обработки</label>
+				<input class="file-input" id="fileInput" type="file" name="file">
+				<button class="button">Загрузить</button>
+			</form>
+
+			@yield('result')
+		</div>
 	</div>
 </body>
 
--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -1,28 +1,29 @@
 @extends('index')
 
 @section('result')
-	<div class="image-section">
-		<div class="image-block">
-			<img class="image" src="{{ $images['original'] }}" alt="Исходное изображение">
-			<p class="description">Исходное изображение</p>
-		</div>
-		<div class="image-block">
-			<img class="image" src="{{ $images['cleaned'] }}" alt="Очищенное изображение">
-			<p class="description">Очищенное изображение</p>
-		</div>
-		<div class="image-block">
-			<img class="image" src="{{ $images['resize'] }}" alt="Палитра">
-			<p class="description">Палитра</p>
-		</div>
-	</div>
-
-	<div class="color-palette">
-		@foreach ($palette as $color)
-			<div class="color-block">
-				<div class="color" style="background-color: #{{ $color }};">
-					<p class="color__code">#{{ $color }}</p>
-				</div>
+	<div>
+		<div class="image-section">
+			<div class="image-block">
+				<img class="image" src="{{ $images['original'] }}" alt="Исходное изображение">
+				<p class="description">Исходное изображение</p>
 			</div>
-		@endforeach
-	</div>
-@endsection
+			<div class="image-block">
+				<img class="image" src="{{ $images['cleaned'] }}" alt="Очищенное изображение">
+				<p class="description">Очищенное изображение</p>
+			</div>
+			<div class="image-block">
+				<img class="image" src="{{ $images['resize'] }}" alt="Палитра">
+				<p class="description">Палитра</p>
+			</div>
+		</div>
+
+		<div class="color-palette">
+			@foreach ($palette as $color)
+				<div class="color-block">
+					<div class="color" style="background-color: #{{ $color }};">
+						<p class="color__code">#{{ $color }}</p>
+					</div>
+				</div>
+			@endforeach
+		</div>
+	@endsection
