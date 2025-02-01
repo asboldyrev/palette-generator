932efaaa602acdd3f576cfa961ae8cf807b0a4f2
asboldyrev <angelcorpc@ya.ru>
2025-02-01 15:41:47 +0700

    Мелкие правки стилей хаба палитры

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,9 +1,9 @@
 @extends('template')
 
 @section('content')
-	<div class="row">
+	<div class="row g-4 mb-5 mt-4">
 		@foreach ($images as $image)
-			<div class="col-sm-2 mb-4">
+			<div class="col-sm-2">
 				<a class="card" style="overflow: hidden;" href="{{ route('images.show', ['id' => $image->fileInfo->id]) }}">
 					<img class="card-img-top result-image" src="{{ $image->paths->originalImage }}" alt="...">
 
@@ -11,7 +11,7 @@
 						<table class="table-sm table-borderless border-top mb-0 table">
 							<tr>
 								@foreach ($palette as $color)
-									<td class="py-3" style="background-color: #{{ $color }};"></td>
+									<td class="py-4" style="background-color: #{{ $color }};"></td>
 								@endforeach
 							</tr>
 						</table>
