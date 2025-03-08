942cf789e1027c9775c50122359300104d8a22e0
asboldyrev <angelcorpc@ya.ru>
2025-03-08 17:05:35 +0700

    Добавил переключение картинок в хабе палитр

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,15 +1,23 @@
 @extends('template')
 
 @section('content')
-	<div class="row g-4 mb-5 mt-4">
+	@if (request('show-images'))
+		<a href="{{ route('images.list') }}">Без картинок</a>
+	@else
+		<a href="{{ route('images.list', ['show-images' => 1]) }}">С картинками</a>
+	@endif
+
+	<div class="row g-5 mb-5 mt-4">
 		@foreach ($images as $image)
 			<div class="col-sm-2">
 				<a class="card" style="overflow: hidden;" href="{{ route('images.show', ['id' => $image->fileInfo->id]) }}">
-					<img class="card-img-top result-image" src="{{ $image->paths->originalImage }}" alt="...">
+					@if (request('show-images'))
+						<img class="card-img-top result-image" src="{{ $image->paths->originalImage }}" alt="...">
+					@endif
 
 					@foreach ($image->palette as $palette)
-						<table class="table-sm table-borderless border-top mb-0 table">
-							<tr>
+						<table class="table-sm table-borderless mb-0 table">
+							<tr @class(['border-top' => !$loop->first])>
 								@foreach ($palette as $color)
 									<td class="py-4" style="background-color: #{{ $color }};"></td>
 								@endforeach
